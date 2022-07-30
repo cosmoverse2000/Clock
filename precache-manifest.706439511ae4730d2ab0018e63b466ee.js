ef2d9286f13da25c48bf52c67bb18993e8f2352b
Anshul Patel <anshulpatel5757@gmail.com>
2022-07-30 14:02:47 +0530

    Updates

---
+++ precache-manifest.706439511ae4730d2ab0018e63b466ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a071f79f24bd0e4cb4e2db347be07b2",
+    "url": "/Clock/index.html"
+  },
+  {
+    "revision": "623f6ab0b4a8eeca0070",
+    "url": "/Clock/static/js/2.ad054d7a.chunk.js"
+  },
+  {
+    "revision": "524cea1c4faaa0ac69f8",
+    "url": "/Clock/static/js/main.9e1d9094.chunk.js"
+  },
+  {
+    "revision": "303a938a94dc58a5049a",
+    "url": "/Clock/static/js/runtime-main.e449ae82.js"
+  }
+]);
